51f57660adc8c6ff1b43879f7e03aed255bdef98
ionut <ionitaionutvictor@gmail.com>
2021-12-14 17:07:50 +0200

    Add project files.

---
+++ Currency_GuessMyNumber/Currency_GuessMyNumber/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Currency_GuessMyNumber
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void NavigateButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+
+            private void Button_Clicked(object sender, EventArgs e)
+        {
+            float Value=0;
+            if (!string.IsNullOrEmpty(EntryValue.Text))
+            {
+                Value = float.Parse(EntryValue.Text);
+                
+            }
+             else
+                PrintValue.Text = "Nu ati introdus corect";
+
+            EntryValue.Text = null;
+            PrintValue.Text =$"{(Value * 4.95).ToString()} Lei  " ;
+        }
+    }
+}
---
+++ Currency_GuessMyNumber/Currency_GuessMyNumber/Page1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace Currency_GuessMyNumber
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(1,100);
+            TheRandomNumber.Text = number.ToString();
+            Image.IsVisible = false;
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            int guess = Convert.ToInt32(Guess.Text);
+            if (guess > Convert.ToInt32(TheRandomNumber.Text))
+                Response.Text = "The number i'm thinking about is smaller than that. Try again";
+            else if (guess < Convert.ToInt32(TheRandomNumber.Text))
+                Response.Text = "The number i'm thinking about is bigger than that. Try again";
+            else if (guess == Convert.ToInt32(TheRandomNumber.Text))
+            {
+                Response.Text = "That's The number, Congratulations";
+                Image.IsVisible = true;
+            }
+
+        }
+
+        
+    }
+}
